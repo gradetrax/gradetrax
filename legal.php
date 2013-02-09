5cb945fff9c8c584b59ad8117976f39f4a32a9ba
Mallory Smith <mallorysmith3@my.unt.edu>
2013-02-09 14:32:58 -0600

    Created a legal page
    More information should be added.

---
+++ legal.php
@@ -0,0 +1,13 @@
+<?php
+require 'header.php';
+?>
+
+<h3>Legal</h3>
+
+<br>GradeTrax will not sell your personal information.  Grades that are posted publicly will
+remain anonymous.
+
+
+<?php
+require 'footer.php';
+?>
