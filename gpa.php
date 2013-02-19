e6859c6e6ac837f2f9d64c028f1b5da74d294471
Mallory Smith <mallorysmith3@my.unt.edu>
2013-02-18 19:19:56 -0600

    Grades and GPA pages

---
+++ gpa.php
@@ -0,0 +1,12 @@
+<?php
+require 'header.php';
+?>
+
+<h3>GPA</h3>
+
+<br>Once you enter grades, you will be able to see your projected GPA for the semester, and your cumulative GPA.
+
+
+<?php
+require 'footer.php';
+?>
---
+++ grades.php
@@ -0,0 +1,11 @@
+<?php
+require 'header.php';
+?>
+
+<h3>Grades</h3>
+
+<br>You will be able to enter grades here shortly.
+
+<?php
+require 'footer.php';
+?>
