00df09ad224e2c6ba66e9b5893994d6b8f652587
Lisa Reynolds <lisareynlds2@my.unt.edu>
2013-02-20 18:08:04 -0600

    Account Creation form
    -When an error occurs, values are remembered
    -When the account is created the user is notified and linked to the
    login page

--- create.php
+++ create.php
@@ -6,42 +6,68 @@ require 'header.php';
 if (isset($_POST['submit'])) {
 
 	if ($_POST['name'] == '') {
-		echo "Please enter a username";
+		echo "Please enter a username<br>";
 	} else if ($_POST['pass'] == '') {
-		echo "Please enter a password";
+		echo "Please enter a password<br>";
 	} else if (preg_match("/^[^0-9][a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[@][a-zA-Z0-9_]*([.][a-zA-Z0-9_]+)*[\.]*unt[\.]edu$/", $_POST['email']) <= 0) {
-		echo "Please enter a valid UNT email";
+		echo "Please enter a valid UNT email<br>";
 	} else if (strpos($_POST['name'], "@") !== false) {
-		echo "Please do not include the @ symbol in your username";
+		echo "Please do not include the @ symbol in your username<br>";
 	} else {
 		$query = "INSERT INTO students (username, password, email) VALUES ('" . $_POST['name'] . "', '" . $_POST['pass'] . "', '" . $_POST['email'] . "')";
 	//	echo $query;
-		if (mysql_query($query))
-			echo "Account created.";
-		else
+		if (mysql_query($query)) {
+			echo <<<EOT
+			Account created.
+			<br><br>
+			<a href="login.php" class="mainButton">Log In</a>
+EOT;
+			$accountMade = TRUE;
+		} else
 			echo "mysql error: " . mysql_error();
 	}
 	
-} else {
-
-?>
-
-<h3>Create an Account</h3>
-
-Note: Your email must end with unt.edu
-<br>(example@my.unt.edu or example@unt.edu)
-<br><br><br>
-
-<form id="accountForm" method="POST" action="">
-Username: <input type="text" name="name" autofocus />
-<br><br>Password: <input type="password" name="pass" />
-<br><br>School Email: <input type="text" name="email" />
-<br><br><input type="submit" name="submit" value="submit">
-</form>
-
-<?php
-
 }
 
+if (!isset($accountMade)) {
+	?>
+
+	<h3>Create an Account</h3>
+
+	Note: Your email must end with unt.edu
+	<br>(example@my.unt.edu or example@unt.edu)
+	<br><br><br>
+
+	<?php
+	
+	// Define default values
+	if (isset($_POST['name']))
+		$name = $_POST['name'];
+	else
+		$name = "";
+		
+	if (isset($_POST['pass']))
+		$pass = $_POST['pass'];
+	else
+		$pass = "";
+		
+	if (isset($_POST['email']))
+		$email = $_POST['email'];
+	else
+		$email = "";
+	
+	
+	// Print the form
+	echo <<<EOT
+	<form id="accountForm" method="POST" action="">
+	Username: <input type="text" name="name" value="$name" autofocus />
+	<br><br>Password: <input type="password" name="pass" value="$pass" />
+	<br><br>School Email: <input type="text" name="email" value="$email" />
+	<br><br><input type="submit" name="submit" value="submit">
+	</form>
+EOT;
+
+}
+	
 require 'footer.php';
 ?>
