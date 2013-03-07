70ab6c748c26e89bca0968bcc3cc826ea0c31747
Mallory Smith <mallorysmith3@my.unt.edu>
2013-03-07 15:53:56 -0600

    user can login with username or email address

--- login.php
+++ login.php
@@ -3,10 +3,18 @@
 $TITLE = "Login";
 require 'header.php';
 
-
+$email =false;
 if (isset($_POST['submit'])) {
-
-	$query = "SELECT * FROM students WHERE username='" . $_POST['name'] . "'";
+$query = "SELECT * FROM students WHERE username='" . $_POST['name'] . "'";
+	for ($i = 0; $i<strlen($_POST['name']); $i++)  { 
+      $character = substr($_POST['name'], $i,1);     
+	  if($character=='@')
+	  {
+		$query = "SELECT * FROM students WHERE email='" . $_POST['name'] . "'";
+		$email=true;
+		}
+}
+	
 	$results = mysql_query($query);
 
 	if ($row = mysql_fetch_array($results)) {
@@ -23,6 +31,9 @@ if (isset($_POST['submit'])) {
 			echo "Invalid password.<br>";
 		}
 	} else {
+		if($email)
+			echo "Email address does not exist.<br>";
+		else
 		echo "Username does not exist.<br>";
 	}
 
@@ -33,7 +44,7 @@ if (isset($_POST['submit'])) {
 <h3>Log In</h3>
 
 <form id="form" action="" method="POST">
-Username: <input type="text" name="name" /autofocus>
+Username or email: <input type="text" name="name" /autofocus>
 <br>Password: <input type="password" name="pass">
 <br><input type="submit" name="submit" value="submit">
 
