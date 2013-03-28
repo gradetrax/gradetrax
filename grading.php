9c977e5506c7df4271134458608f4dcca7157f15
Mallory Smith <mallorysmith3@my.unt.edu>
2013-03-28 12:58:12 -0500

    User can edit and delete categories

--- grading.php
+++ grading.php
@@ -20,6 +20,47 @@ if(isset($_POST['submit'])) {
 }
 
 
+
+if(isset($_POST['update'])) {
+
+	if (($_POST['name'] != "") && (is_numeric($_POST['weight']))) {
+		
+		$query = "UPDATE categories SET name='" . $_POST['name'] . "', weight=" . $_POST['weight'] . " WHERE name='" . $_POST['category'] . "'";
+//		echo $query;
+		if (!mysql_query($query)) {
+			echo "Update failed: $query";
+			echo "<br>" . mysql_error();
+		}
+		
+	} else {
+		echo "Please enter a category name and numeric weight.<br><br>";
+	}
+	
+}
+
+
+
+if(isset($_POST['delete'])) {
+
+	if (($_POST['name'] != "") && (is_numeric($_POST['weight']))) {
+		
+		$query = "DELETE FROM categories WHERE name='" . $_POST['category'] . "' AND courseID=". $_GET['id'];
+//		echo $query;
+		if (!mysql_query($query)) {
+			echo "Update failed: $query";
+			echo "<br>" . mysql_error();
+		}
+		
+	} else {
+		echo "Please enter a category name and numeric weight.<br><br>";
+	}
+	
+}
+
+
+
+
+
 $query = "SELECT * FROM courses WHERE id = " . $_GET['id'];
 if (!($course = mysql_query($query)))
 	die("Error with query: $query");
@@ -36,12 +77,31 @@ while($cat = mysql_fetch_array($cats)) {
 	echo <<<EOT
 	<p class="listItem" onclick="show('$cat[name]')">$cat[weight]%: $cat[name]</p>
 	<p class="$cat[name]" style="display: none">lol</p>
+	<button class="$cat[name]"  id="edit$cat[name]" onClick="show('edit$cat[name]')"style="display: none">edit </button>
+	
+	<form action="" method="POST" class="edit$cat[name]" style = "display:none;">
+	Category Name<input type="text" name="name" value="$cat[name]" onblur="if (this.value == '') {this.value = '$cat[name]';}"
+	onfocus="if (this.value == '$cat[name]') {this.value = '';}" />
+	<br>Percentage of Grade<input type="text" name="weight" value="$cat[weight]" onblur="if (this.value == '') {this.value = '$cat[weight]';}"
+	onfocus="if (this.value == '$cat[weight]') {this.value = '';}" />
+	<input type='hidden' value='$cat[name]' name='category'>
+	<br><input type="submit" class="editBox" name="update" value="update" />
+	<br><input type="submit" class="editBox" name="delete" value="delete" />
+	</form>
+	
+
+	
+	
+	
 EOT;
 $weightTotal += $cat['weight'];
 }
 if ($weightTotal < 100)
 	echo "Warning: Your grade percentages do not add up to 100.";
 ?>
+
+
+
 
 
 <script>
