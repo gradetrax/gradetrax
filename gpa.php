a17e711dda9730e437d52a19402d2ad0f7881e41
Mallory Smith <mallorysmith3@my.unt.edu>
2013-03-28 19:04:37 -0500

    Completed Courses GPA can be calculated

--- gpa.php
+++ gpa.php
@@ -4,7 +4,42 @@ require 'header.php';
 
 <h3>GPA</h3>
 
-<br>Once you enter grades, you will be able to see your projected GPA for the semester, and your cumulative GPA.
+<?php
+$total=0;
+$credits=0;
+$query = "SELECT * FROM completed_courses WHERE username='" . $_SESSION['username']."'" ;
+if ($results = mysql_query($query)) {
+
+	$courses = array();
+	$row = mysql_fetch_array($results);
+	if (is_array($row)) {
+
+		$courses[] = $row;
+
+		while ($row = mysql_fetch_array($results)) {
+			$courses[] = $row;
+		}
+			
+		echo "<ul style='padding: 0px'>";
+		foreach ($courses as $course) {
+				//echo $course["course"];
+				//echo $course["credits"];
+				$total=$total+($course["credits"]*$course["grade"]);
+				$credits=$credits+$course["credits"];
+		}
+		
+		$completedGPA=number_format($total/$credits,2);
+		
+			echo "Your GPA on past course work is " . $completedGPA;
+	} else {
+		echo "You have no completed courses saved.";
+	}
+
+}
+?>
+
+
+
 
 <br><br>
 <a href="finishedCourses.php" class="mainButton">Add Completed Course</a>
