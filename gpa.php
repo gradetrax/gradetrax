e2af4b15a5caf0b9b42f5fa1fdaa34659d24f8d6
Lisa Reynolds <lisareynlds2@my.unt.edu>
2013-03-30 12:21:44 -0500

    Visual changes to GPA page
    (Sorry for the awful colors.)

--- gpa.php
+++ gpa.php
@@ -15,41 +15,51 @@ if ($results = mysql_query($query)) {
 	$row = mysql_fetch_array($results);
 	if (is_array($row)) {
 
+		// Add first course
 		$courses[] = $row;
-echo "<table border='1'>";
-	echo "<tr>";
+
+		while ($row = mysql_fetch_array($results)) {
+			$courses[] = $row; // Add the rest of the courses
+			
+			// Sum hours and credits:
+			// echo "Course : " . $course["course"] . " Credits : " . $course["credits"] . " Grade : " . $course["grade"]. "<br>";
+			$total=$total+($row["credits"]*$row["grade"]);
+			$credits=$credits+$row["credits"];
+		}
+
+		// Calculate and print GPA
+		$completedGPA=number_format($total/$credits,3);
+		// echo "Your GPA on past coursework is " . $completedGPA;
+		
+		// Testing
+		echo "<span id='completedGPA'>Completed Coursework<br>" . $completedGPA . "</span>";
+		echo "<span id='semesterGPA'>Projected Semester<br>{Coming Soon}</span>";
+		echo "<span id='projectedGPA'>Projected Total<br>{Coming Soon}</span>";
+		
+		echo "<div class='clearfloat'></div>";
+		// /Testing
+		
+		echo "<br><br><br>";
+
+		
+		// Start table
+		echo "<table border='1' cellspacing='0' cellpadding='5'>";
+		echo "<tr>";
 		echo "<th>Course Name</th>";
 		echo "<th>Credits</th>";
 		echo "<th>Grade</th>";
 		echo "</tr>";
-	
-		while ($row = mysql_fetch_array($results)) {
-			$courses[] = $row;
-		}
-			
-		echo "<ul style='padding: 0px'>";
+		// echo "<ul style='padding: 0px'>";
+		// Print each course as a row
 		foreach ($courses as $course) {
-		
-		
-			
-		
-		echo "<tr>";
-		echo "<td>".$course['course']."</td>";
-		echo "<td>".$course['credits']."</td>";
-		echo "<td>".$course['grade']."</td>";
-		echo "</tr>";
-	
-			
-
-//			echo "Course : " . $course["course"] . " Credits : " . $course["credits"] . " Grade : " . $course["grade"]. "<br>";
-				$total=$total+($course["credits"]*$course["grade"]);
-				$credits=$credits+$course["credits"];
-		}
+			echo "<tr>";
+			echo "<td>".$course['course']."</td>";
+			echo "<td>".$course['credits']."</td>";
+			echo "<td>".$course['grade']."</td>";
 			echo "</tr>";
+		}
 		echo "</table>";
-		$completedGPA=number_format($total/$credits,3);
-		
-			echo "<br><br><br>Your GPA on past course work is " . $completedGPA;
+
 	} else {
 		echo "You have no completed courses saved.";
 	}
