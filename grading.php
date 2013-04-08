3bccdeea3371f1c2cfcd7b7245f172646285e9b6
Lisa Reynolds <lisareynlds2@my.unt.edu>
2013-04-08 15:18:39 -0500

    Categories now list assignments
    Also calculates the category average. Looks kind of messy...

--- grading.php
+++ grading.php
@@ -93,14 +93,29 @@ if ($_SESSION['username'] != $course['username']) // The courseID was not for a 
 $query = "SELECT * FROM categories WHERE courseid=" . $_GET['id'];
 if (!($cats = mysql_query($query))) // Query failed
 	die("Error with query: $query");
+	
+
+// Returns all assignments for this course
+$query = "SELECT * FROM assignments WHERE courseid=" . $_GET['id'];
+if (!($as = mysql_query($query))) // Query failed
+	die("Error with query: $query");
+$assignments = array();
+// Add each assignment to array
+while ($row = mysql_fetch_array($as)) {
+	$assignments[] = $row;
+}
 
 
 // Print information and edit, update, and delete buttons for each category
 while($cat = mysql_fetch_array($cats)) {
+	$catAverage = 0;
+	$catAssignments = 0;
+	
 	echo <<<EOT
 	<p class="listItem" onclick="show('$cat[name]')">$cat[weight]%: $cat[name]</p>
-	<!-- <p class="$cat[name]" style="display: none">lol</p> -->
-	<button class="$cat[name]"  id="edit$cat[name]" onClick="show('edit$cat[name]')"style="display: none">edit</button>
+	<p class="$cat[name]" style="display: none"><br></p>
+	<!-- Edit form -->
+	<button class="$cat[name]"  id="edit$cat[name]" onClick="show('edit$cat[name]')"style="display: none">Edit Category</button>
 	<form action="" method="POST" class="edit$cat[name]" style = "display:none;">
 	Category Name<input type="text" name="name" value="$cat[name]" onblur="if (this.value == '') {this.value = '$cat[name]';}"
 	onfocus="if (this.value == '$cat[name]') {this.value = '';}" />
@@ -111,7 +126,35 @@ while($cat = mysql_fetch_array($cats)) {
 	<input type="submit" class="editBox" name="delete" value="delete" />
 	<br><br>
 	</form>
+	<!-- /Edit form -->
+	<p class="$cat[name]" style="display: none"><br></p>
+	<!-- Assignments -->
+	<table class="$cat[name]" style="display: none">
+EOT;
+
+	foreach($assignments as $assignment) {
+		if ($assignment['categoryID'] == $cat['id']) {
+			echo "<tr>";
+			echo "<td style='padding-right:20px;'>$assignment[name]</td>";
+			if ($assignment['grade'] >= 0) { // Don't print grades for ungraded assignments
+				// Increment average counters
+				$catAverage += $assignment['grade'];
+				$catAssignments++;
+				echo "<td>$assignment[grade]</td>";
+			}
+			echo "</tr>";
+		}
+	}
+	
+	// Calculate average
+	$catAverage = number_format($catAverage / $catAssignments, 2);
+	
+	echo <<<EOT
+	</table>
+	<p class="$cat[name]" style="display: none"><strong>Category average: </strong>$catAverage</p>
+	<!-- /Assignments -->
 	<br>
+	<p class="$cat[name]" style="display: none"><br></p>
 EOT;
 
 $weightTotal += $cat['weight']; // Add weight to total
