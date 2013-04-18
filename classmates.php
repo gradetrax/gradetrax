3ed8ee31e8e66f88883da3b4085e4b8cace46731
Lisa Reynolds <lisareynlds2@my.unt.edu>
2013-04-18 13:47:49 -0500

    Friends part two
    So it's still totally broken but you can send and reject requests.
    Accepting not so much.

--- classmates.php
+++ classmates.php
@@ -6,9 +6,62 @@ require 'header.php';
 <a href='newClassmate.php' class='mainButton'>Send Request</a>
 <br><br><br><br>
 
-<!-- Start column one -->
-<div style='float:left; margin-right:60px;'>
-<h4>Accepted Classmates</h4>
+
+<div>
+<h4>Requests</h4>
+
+
+<?php
+
+// Returns all listed classmates
+$query = "SELECT * FROM requests WHERE friendname='" . $_SESSION['username'] . "'";
+if (!($result = mysql_query($query))) { // Query failed
+	echo "Query error: " . mysql_error();
+	require 'footer.php';
+	die();
+}
+
+echo "<table cellpadding='5px'>";
+
+// Loop for each friend
+while ($friend = mysql_fetch_array($result)) {
+	echo "<tr>";
+	
+	// Print friend's username
+	echo "<td style='padding-right: 20px;'><a href='request.php?id=$friend[id]' class='listItem'>$friend[username]</a></td>";
+	// Separate shared courses by ; and store in array
+	$courses = explode(";", $friend['courses']);
+
+	// Print courses
+	foreach($courses as $course) {
+		// Blank courses show up for some reason
+		if ($course == '')
+			break;
+	
+		// Return course name
+		$query = "SELECT course FROM courses WHERE id=" . $course;
+		if (!($course = mysql_query($query))) { // Query failed
+			echo "Query error: " . mysql_error();
+			require 'footer.php';
+			die();
+		}
+		// Print course name
+		$course = mysql_fetch_array($course);
+		echo "<td style='padding-right: 20px;'>$course[course]</td>";
+	}
+
+	echo "</tr>";
+}
+
+echo "</table>";
+?>
+
+</div>
+
+<br><br><br><br>
+
+<div>
+<h4>Classmates</h4>
 
 
 <?php
@@ -34,6 +87,10 @@ while ($friend = mysql_fetch_array($result)) {
 
 	// Print courses
 	foreach($courses as $course) {
+		// Blank courses show up for some reason
+		if ($course == '')
+			break;
+
 		// Return course name
 		$query = "SELECT course FROM courses WHERE id=" . $course;
 		if (!($course = mysql_query($query))) { // Query failed
@@ -53,12 +110,8 @@ echo "</table>";
 ?>
 </div>
 
-<!-- Start column two -->
-<div>
-<h4>Requests</h4>
 
 
-</div>
 
 <?php
 require 'footer.php';
---
+++ request.php
@@ -0,0 +1,84 @@
+<?php
+require 'header.php';
+
+// Get request in question
+$query = "SELECT * FROM requests WHERE id=" . $_GET['id'];
+if (!($result = mysql_query($query))) { // Query failed
+	die("Query error: " . mysql_error);
+}
+$request = mysql_fetch_array($result); // Store result in array
+
+
+if (isset($_POST['reject'])) { // Remove request from database
+	$query = "DELETE FROM requests WHERE id=" . $_GET['id'];
+	if (!mysql_query($query)) { // Query error
+		die("Delete error: " . mysql_error());
+	}
+	?>
+	<script language="JavaScript">
+		window.location = "classmates.php";
+	</script>
+	<?php
+} else if (isset($_POST['accept'])) { // Remove from requests, insert to classmates
+	// Delete from requests
+	$query = "DELETE FROM requests WHERE id=" . $_GET['id'];
+	if (!mysql_query($query)) { // Query error
+		die("Delete error: " . mysql_error());
+	}
+	
+	// Insert to classmates
+	$query = "INSERT INTO classmates (username, friendname, courses) VALUES ('" . $request['username'] . "', '" . $request['friendname'] . "', '" . $request['courses'] . "')";
+	echo $query . "<br>";
+	if (!mysql_query($query)) { // Query error
+		die("Insert error: " . mysql_error());
+	}
+	?>
+	<script language="JavaScript">
+		window.location = "classmates.php";
+	</script>
+	<?php
+}
+
+
+
+echo "<h3>Classmate Request from $request[username]</h3>";
+?>
+
+<br>
+<strong><?php echo $request['username']; ?></strong> would like to share grades in these classes:
+<br><br>
+
+<ul>
+<?php
+	$courses = explode(";", $request['courses']);
+
+	// Print courses
+	foreach($courses as $course) {
+		// Blank courses show up for some reason
+		if ($course == '')
+			break;
+	
+		// Return course name
+		$query = "SELECT * FROM courses WHERE id=" . $course;
+		if (!($course = mysql_query($query))) { // Query failed
+			echo "Query error: " . mysql_error();
+			require 'footer.php';
+			die();
+		}
+		// Print course name
+		$course = mysql_fetch_array($course);
+		echo "<li style='margin-left: 2.5em;'>$course[course]</li>";
+	}
+?>
+</ul>
+
+<br><br>
+<form action="" method="post">
+<input type='submit' name='accept' value='Accept' />
+<input type='submit' name='reject' value='Reject' />
+</form>
+
+
+<?php
+require 'footer.php';
+?>
