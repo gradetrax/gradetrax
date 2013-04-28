d2598d57344e32ace0d7111b28a477987e5db2c7
Mallory Smith <mallorysmith3@my.unt.edu>
2013-04-28 15:26:56 -0500

    Added pending requests to Classmates page

--- classmates.php
+++ classmates.php
@@ -8,6 +8,65 @@ require 'header.php';
 <br><br><br><br>
 
 
+
+
+<div>
+<h4>Pending Requests</h4>
+
+
+<?php
+
+// Returns all listed classmates
+$query = "SELECT * FROM requests WHERE username='" . $_SESSION['username'] . "'";
+if (!($result = mysql_query($query))) { // Query failed
+	echo "Query error: " . mysql_error();
+	require 'footer.php';
+	die();
+}
+
+echo "<table cellpadding='5px'>";
+
+// Loop for each friend
+while ($friend = mysql_fetch_array($result)) {
+	echo "<tr>";
+	
+	// Print friend's username
+	echo "<td style='padding-right: 20px;'>$friend[friendname]</a></td>";
+	// Separate shared courses by ; and store in array
+	$courses = explode(";", $friend['courses']);
+
+	// Print courses
+	foreach($courses as $course) {
+		// Blank courses show up for some reason
+		if ($course == '')
+			break;
+	
+		// Return course name
+		$id = array();
+		preg_match("/([a-zA-Z]+)(\\d+)/", $course, $id);
+		$query = "SELECT course FROM courses WHERE department='" . $id[1] . "' AND number=" . $id[2];
+		if (!($course = mysql_query($query))) { // Query failed
+			echo "Query error: " . mysql_error();
+			require 'footer.php';
+			die();
+		}
+		// Print course name
+		$course = mysql_fetch_array($course);
+		echo "<td style='padding-right: 20px;'>$course[course]</td>";
+	}
+
+	echo "</tr>";
+}
+
+echo "</table>";
+?>
+
+</div>
+
+<br><br><br><br>
+
+
+
 <div>
 <h4>Requests</h4>
 
